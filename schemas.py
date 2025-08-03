219768cf3a8af66613ff0687f97dccf86ebd0963
architech66 <104875975+architech66@users.noreply.github.com>
2025-08-03 12:05:42 -0700

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,31 +1,30 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import Optional
 
 class UserCreate(BaseModel):
     username: str
     password: str
     is_admin: bool = False
-    twilio_sid: Optional[str] = None
-    twilio_token: Optional[str] = None
+    twilio_sid: str | None = None
+    twilio_token: str | None = None
 
 class UserOut(BaseModel):
     id: int
     username: str
     is_admin: bool
     active: bool
-    twilio_sid: Optional[str]
-    twilio_token: Optional[str]
-    class Config:
-        orm_mode = True
+    twilio_sid: str | None
+    twilio_token: str | None
+
+    model_config = { "from_attributes": True }
 
 class SessionOut(BaseModel):
     id: int
     user_id: int
     ip_address: str
     timestamp: datetime
-    class Config:
-        orm_mode = True
+
+    model_config = { "from_attributes": True }
 
 class Token(BaseModel):
     access_token: str
