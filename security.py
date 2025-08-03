60e8dcaa52594b2ae9a6f9cc410457eecf0edbdd
architech66 <104875975+architech66@users.noreply.github.com>
2025-08-03 13:43:49 -0700

    security.py

--- security.py
+++ security.py
@@ -1,28 +1,8 @@
 import os
-from passlib.context import CryptContext
-from datetime import datetime, timedelta
-from jose import JWTError, jwt
-from typing import Optional
-from pydantic import BaseModel
+from dotenv import load_dotenv
 
-SECRET_KEY = os.getenv("SECRET_KEY", "f2487a44fee137302e164195684a3d99")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
+load_dotenv()
 
-pwd_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = "Bearer"
-
-def get_password_hash(pw: str) -> str:
-    return pwd_ctx.hash(pw)
-
-def verify_password(plain: str, hashed: str) -> bool:
-    return pwd_ctx.verify(plain, hashed)
-
-def create_access_token(data: dict, expires_delta: timedelta = None):
-    to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-class TokenData(BaseModel):
-    username: Optional[str] = None
+ADMIN_USER = os.getenv("ADMIN_USER")
+ADMIN_PASS = os.getenv("ADMIN_PASS")
+SESSION_SECRET = os.getenv("SESSION_SECRET")
