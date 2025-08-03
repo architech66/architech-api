d830ed3c33838c59e9be77727f11b7b512e38eb5
architech66 <104875975+architech66@users.noreply.github.com>
2025-08-03 14:44:11 -0700

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,9 @@
+# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQL_URL = "sqlite:///./app.db"
-engine   = create_engine(SQL_URL, connect_args={"check_same_thread": False})
+SQL_URL    = "sqlite:///./app.db"
+engine     = create_engine(SQL_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base     = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+Base       = declarative_base()
