8a35276633c1201fe84402c8222bebce28949d38
architech66 <104875975+architech66@users.noreply.github.com>
2025-08-03 00:26:57 -0700

    database.py
    v3

--- database.py
+++ database.py
@@ -1,19 +1,16 @@
-# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./app.db"
+SQL_URL = "sqlite:///./app.db"
+engine = create_engine(SQL_URL, connect_args={"check_same_thread": False})
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency for FastAPI
+# Dependency
 def get_db():
-    db: Session = SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
